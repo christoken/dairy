e67d1b84a907332d20a0360ca91946f21ec370c4
christoken <christoken@users.noreply.github.com>
2018-11-19 15:06:20 +0300

    Delete db.php

--- dairyserver/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$newdbname=$_SESSION['orgtel'];
-//echo $newdbname;
-$con = mysqli_connect('localhost','root','Threemodern789',$newdbname);
-if (mysqli_connect_errno()) {
-  echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
-  }  
- ?>
